eb71c12e9be29f0626d028888d133f295fd276b6
Mónica Waterhouse <mwaterhouse@estudiantec.cr>
2020-04-11 15:42:42 -0600

    PD-8 Create color pattern #9

--- src/SimonGame/SimonController.java
+++ src/SimonGame/SimonController.java
@@ -12,18 +12,25 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import java.util.concurrent.TimeUnit;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import java.awt.*;
+import java.lang.reflect.Array;
 
 public class SimonController {
-
-    int round = 1;
 
     @FXML
     Stage primaryStage;
     Button buttonPressed;
-    int colorCode;
-
+    String[] buttonColors = new String[]{"btn_Pink", "btn_Purple", "btn_Green", "btn_Orange", "btn_Blue"};
+    Button btn_Pink;
+    Button btn_Purple;
+    Button btn_Green;
+    Button btn_Blue;
+    Button btn_Orange;
 
 
     public void closeWindow(MouseEvent mouseEvent) {
@@ -36,6 +43,71 @@ public class SimonController {
         primaryStage.close();
 
     }
+
+    public String pickColor(String[] buttonColors){
+
+        Random colorPlace = new Random();
+        int colorSelected = colorPlace.nextInt(4);
+        String colorName = buttonColors[colorSelected];
+        return colorName;
+
+    }
+
+    public ArrayList<String> flashColors(String colorName, ArrayList<String> pattern) throws InterruptedException {
+
+        pattern.add(colorName);
+        String color;
+
+        for (int i=0; i < pattern.size(); i++){
+            color = pattern.get(i);
+
+            if (color.equals("btn_Pink")){
+
+                btn_Pink.setStyle("-fx-background-color: #CC3366; ");
+                TimeUnit.SECONDS.sleep(1);
+                btn_Pink.setStyle("-fx-background-color: #96274c; ");
+
+
+            }
+
+            else if (color.equals("btn_Purple")) {
+
+                btn_Purple.setStyle("-fx-background-color: #9B59B6; ");
+                TimeUnit.SECONDS.sleep(1);
+                btn_Purple.setStyle("-fx-background-color: #6f4082; ");
+
+            }
+
+            else if (color.equals("btn_Green")) {
+
+                btn_Green.setStyle("-fx-background-color: #CDDC39; ");
+                TimeUnit.SECONDS.sleep(1);
+                btn_Green.setStyle("-fx-background-color: #7d8622; ");
+
+            }
+
+            else if(color.equals("btn_Orange")) {
+
+                btn_Orange.setStyle("-fx-background-color: #FFA000; ");
+                TimeUnit.SECONDS.sleep(1);
+                btn_Orange.setStyle("-fx-background-color: #9d6202; ");
+
+            }
+
+            else if(color.equals("btn_Blue")) {
+
+                btn_Blue.setStyle("-fx-background-color: #00ACC1; ");
+                TimeUnit.SECONDS.sleep(1);
+                btn_Blue.setStyle("-fx-background-color: #005c64; ");
+
+            }
+
+        }
+
+        return pattern;
+
+    }
+
 
 
     public void pressButton(ActionEvent actionEvent) {
