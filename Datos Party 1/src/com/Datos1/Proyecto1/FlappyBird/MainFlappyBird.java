5fa63912bd3ea4cdf79bdb445c7ba4f69ec69b4c
Mónica Waterhouse <mwaterhouse@estudiantec.cr>
2020-06-01 12:43:16 -0600

    PD-68 Create Cover and game instructions screen for Flappy Bird

--- Datos Party 1/src/com/Datos1/Proyecto1/FlappyBird/MainFlappyBird.java
+++ Datos Party 1/src/com/Datos1/Proyecto1/FlappyBird/MainFlappyBird.java
@@ -1,8 +1,10 @@
 package com.Datos1.Proyecto1.FlappyBird;
 
+import com.Datos1.Proyecto1.cover.Cover;
 import com.Datos1.Proyecto1.cover.CoverEvent;
 
 import javax.swing.JFrame;
+import java.io.IOException;
 
 public class MainFlappyBird {
 	/**
@@ -12,20 +14,47 @@ public class MainFlappyBird {
 	 */
 	public static int players=4;
 	static boolean startPlaying = false;
+
 	
 	public MainFlappyBird(int players) {
 		this.players=players;
 	}
+
+
 	
-	
-	public static void main (String[] args) {
+	public static void main (String[] args) throws IOException {
+
+		String pathLogo = "images/fBLogo.png";
+		String pathEnterPress = "images/enterPress.png";
+		String pathSpacePress = "images/spacePress.png";
+		String pathInstructions = "images/fBInstructions.png";
+		String pathBackground = "images/WallpaperFB.jpg";
 
 		MainFlappyBird main=new MainFlappyBird(players);
 
+		Cover fbCover = new Cover(pathLogo,pathBackground,pathEnterPress,pathSpacePress,pathInstructions);
+		fbCover.createWindow();
 
-		Window window=new Window(players);
-		window.setVisible(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while(!startPlaying){
+
+			System.out.println("while");
+
+			if(CoverEvent.closeCover){
+				startPlaying = true;
+			}
+		}
+
+		if (startPlaying){
+
+			fbCover.getWindow().setVisible(false);
+			fbCover.getWindow().dispose();
+
+			Window window=new Window(players);
+			window.setVisible(true);
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+
+
 	}
 
 }
